5f3694e3253e7d012bc23db0619d6d5cfe28a329
rurero2 <0121mrei@gmail.com>
2024-10-23 03:48:32 +0900

    production環境でのプリコンパイルを記述

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
+
 bundle install
 bundle exec rake assets:precompile
+bundle exec rake assets:precompile RAILS_ENV=production
 bundle exec rake assets:clean
 bundle exec rake db:migrate
