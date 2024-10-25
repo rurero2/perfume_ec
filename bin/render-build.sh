1c42ffc5c730038fe076881272fe685f33e00628
rurero2 <0121mrei@gmail.com>
2024-10-26 00:04:21 +0900

    render側で行う項目を削除

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,4 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rake db:seed
