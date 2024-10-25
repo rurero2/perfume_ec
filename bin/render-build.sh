5a94d6743582613a7fa1680ab7feda42f49e2408
rurero2 <0121mrei@gmail.com>
2024-10-26 01:41:08 +0900

    assets.rbのpathをnode_modulesに

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,3 +7,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+#bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate && bundle exec rails db:seed
+#bundle install && bundle exec rake assets:precompile && bundle exec rake assets:clean
+#./bin/render-build.sh
