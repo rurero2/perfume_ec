f0f5be9a9da36308ae0cb0c8315b24097a9e2153
rurero2 <0121mrei@gmail.com>
2024-10-25 23:53:03 +0900

    ビルド時にサンプルデータ挿入

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,4 +6,6 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+bundle exec rake db:create
 bundle exec rake db:migrate
+bundle exec rake db:seed
