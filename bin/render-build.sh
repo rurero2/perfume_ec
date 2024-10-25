90d1c316695d76705ffdf39353e8b1b8ba2c3878
rurero2 <0121mrei@gmail.com>
2024-10-26 02:01:34 +0900

    build.shを再使用

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,5 @@
-bin/render-build.sh内
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-
-#bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate && bundle exec rails db:seed
-#bundle install && bundle exec rake assets:precompile && bundle exec rake assets:clean
-#./bin/render-build.sh
+export YARN_CACHE_FOLDER=/opt/render/.cache/yarn
+export NODE_OPTIONS=--prefix /opt/render/.yarn
+yarn install --check-files
+yarn build
